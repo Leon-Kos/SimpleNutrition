2a1565ba6a55eadcbaff1c948a12ec253c71e44e
Leon-Kos <leonkos.lk@protonmail.com>
2025-09-07 17:53:12 +0200

    Basis Funktionen passen, es kann gescannt werden

--- SimpleNutrition/FoodRequest.swift
+++ SimpleNutrition/FoodRequest.swift
@@ -27,35 +27,4 @@ class FoodRequest {
             }
         }.resume()
     }
-    
-        
-    static func searchProducts(keyword: String, completion: @escaping (Result<[FoodResponse], Error>) -> Void) {
-        let query = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? keyword
-        let urlString = "https://world.openfoodfacts.org/api/v2/search?search_terms=\(query)&page_size=20&fields=code,product_name,brands,quantity,categories,nutriscore_grade,image_url,energy-kcal_100g,fat_100g,saturated-fat_100g,carbohydrates_100g,fiber_100g,sugars_100g,proteins_100g,salt_100g"
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                DispatchQueue.main.async { completion(.failure(error)) }
-                return
-            }
-            guard let data = data else { return }
-            
-            do {
-                struct SearchResponse: Decodable {
-                    let products: [FoodResponse]
-                }
-                let result = try JSONDecoder().decode(SearchResponse.self, from: data)
-                DispatchQueue.main.async {
-                    completion(.success(result.products))
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-            }
-        }.resume()
-    }
-
 }
